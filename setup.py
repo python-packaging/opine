461247c0bc4990b3fa1103c2f4eb04eb60409edb
Tim Hatch <tim@timhatch.com>
2020-03-11 10:39:20 -0700

    Run opine on itself!
    (also bump imperfect to just-release 0.2.0 with `.text` property)

--- setup.py
+++ setup.py
@@ -1,11 +1,3 @@
 from setuptools import setup
 
-setup(
-    use_scm_version=True,
-    install_requires=[
-        "libcst>=0.3.2",
-        "pkginfo>=1.5.0.1",
-        "imperfect>=0.1.1",
-        "moreorless>=0.3.0",
-    ],
-)
+setup(use_scm_version=True)
